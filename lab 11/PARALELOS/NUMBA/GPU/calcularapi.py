00f549aa3a4f97fc1ab34295ced936158279bf93
Abigail178 <74110124+Abigail178@users.noreply.github.com>
2023-01-18 13:18:33 -0600

    Update calcularapi.py

--- lab 11/PARALELOS/NUMBA/GPU/calcularapi.py
+++ lab 11/PARALELOS/NUMBA/GPU/calcularapi.py
@@ -5,13 +5,13 @@ from __future__ import print_function, absolute_import
 from numba import cuda
 from numba.cuda.random import create_xoroshiro128p_states 
 from numba.cuda.random import xoroshiro128p_uniform_float64
-import numby as np 
+import numpy as np 
 import random 
 
 
 @cuda.jit 
 
-def calculadorapi_karnel(rng_states. iteraciones, out):
+def calcularpi_kernel(rng_states, iteraciones, out):
     """Encontrar el valor máximo en  value y guardarlo en result [0]"""
     ii = cuda.grid(1)
 
@@ -40,7 +40,7 @@ bloques = 24
 seed1 = random.uniform(-1,1)
 seed2 = random.seed(seed1)
 seed  = random.randint(0,1000)
-rng_states = create_xoroshiro128p_states(hilosporbloque*bloques,seed)
+rng_states = create_xoroshiro128p_states(hilosporbloque*bloques, seed)
 out = np.zeros(hilosporbloque*bloques, dtype=np.float64)
 
 
@@ -48,5 +48,5 @@ out = np.zeros(hilosporbloque*bloques, dtype=np.float64)
 # Llamar a la función 
 #===========================
 
-calcular.pi_kernel[bloques, hilosporbloque](rng_states,1000, out)
-print('pi:'out_mean())
+calcularpi_kernel[bloques, hilosporbloque](rng_states, 1000, out)
+print('pi:', out.mean())
